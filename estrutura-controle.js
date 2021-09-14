07eb274bdb3095571ec39955c6b92fdacdef5c87
Bruno Marchis <bbbrunomarchis@gmail.com>
2021-09-14 10:20:08 -0300

    Removido os comentários do fonte

--- estrutura-controle.js
+++ estrutura-controle.js
@@ -1,17 +1,4 @@
-/*escreval("Digite o nome do aluno: ")
-leia(nome)
-escreval("Digite a nota 01 do aluno: ")
-leia(nota01)
-escreval("Digite a nota 02 do aluno: ")
-leia(nota02)
-media := (nota01 + nota02) / 2
-escreva(media)
 
-se media >= 50 entao
-  escreval(" Aprovado!", nome)
-senao
-  escreval(" Reprovado!", nome)
-fimse*/
 
 var nome, nota01, nota02, nota03, nota04, nota05, media;
 nome = prompt("Digite o nome do Aluno: ")
--- operacoes-logicas.js
+++ operacoes-logicas.js
@@ -1,23 +1,4 @@
-/*
-passou := falso
-   escreval("Digite o nome do aluno: ")
-   leia(nome)
-   escreval("Digite a nota 01 do aluno: ")
-   leia(nota01)
-   escreval("Digite a nota 02 do aluno: ")
-   leia(nota02)
-   media := (nota01 + nota02) / 2
-   escreva(media)
 
-   se media >= 5 entao
-      passou := verdadeiro
-
-   se ( passou ) && ( media >=5 || media <= 7) entao
-     escreval(" Aprovado!", nome)
-   senao
-     escreval(" Reprovado!", nome)
-   fimse
-   */
 
 var nome, nota01, nota02, nota03, nota04, nota05, media, passou;
 
